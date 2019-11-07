10bbedc3f8872ac7c632aa178aa896d8f2cf3dfe
GABRIEL PEREZ <gabriel1994.gp@gmail.com>
2019-11-07 02:13:41 +0000

    Automatically backed up by Learn

--- parrot.rb
+++ parrot.rb
@@ -1,6 +1,13 @@
+<<<<<<< HEAD
 def parrot(phrase ="Squawk!")
+=======
+def parrot(phrase ="Squawk!","Pretty bird!")
+>>>>>>> 04af86371da4c8fca6f2ac66c3518141a784089b
   puts phrase
   phrase
 end
 
+<<<<<<< HEAD
 phrase = parrot("Pretty bird!")
+=======
+>>>>>>> 04af86371da4c8fca6f2ac66c3518141a784089b
